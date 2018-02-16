2bbcdac59333f49230beffe60de881aefc5c91f4
bilal684 <bilal.itani1234@gmail.com>
2018-02-16 14:35:15 -0500

    Added implementation of sorting & val factors

--- TP3/ListeChaineeProject/src/main/ListeChainee.java
+++ TP3/ListeChaineeProject/src/main/ListeChainee.java
@@ -4,5 +4,5 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public interface ListeChainee {
-	public MyList build(String op,ArrayList<Object> val1, ArrayList<Object> val2)throws IOException;
+	public MyList build(String op,ArrayList<Object> val1, ArrayList<Object> val2, boolean sort, int val1Factor, int val2Factor)throws IOException;
 }
--- TP3/ListeChaineeProject/src/main/ListeChaineeImpl.java
+++ TP3/ListeChaineeProject/src/main/ListeChaineeImpl.java
@@ -7,14 +7,31 @@
 package main;
 
 import java.io.IOException;
+import java.sql.DatabaseMetaData;
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class ListeChaineeImpl implements ListeChainee {
 	
-	public MyList build(String op,ArrayList<Object> val1, ArrayList<Object> val2) throws IOException{
+	public MyList build(String op,ArrayList<Object> val1, ArrayList<Object> val2, boolean ascendingSort, int val1Factor, int val2Factor) throws IOException{
+		
+		if(val1Factor < 0 || val1Factor > 3 || val2Factor < 0 || val2Factor > 3)
+		{
+			throw new IllegalArgumentException("La valeur de val1Factor et val2Factor doit etre entre 0 et 3.");
+		}
 		
 		
 		StringBuilder chaineContent=new StringBuilder();
+		
+		for(int i = 0; i < val1.size(); i++)
+		{
+			val1.set(i, (Integer)val1.get(i) * val1Factor);
+		}
+		
+		for(int i = 0; i < val2.size(); i++)
+		{
+			val2.set(i, (Integer)val2.get(i) * val2Factor);
+		}
 		
 		MyList list = new MyListImpl();	
 		
@@ -69,7 +86,23 @@ public class ListeChaineeImpl implements ListeChainee {
 		   default:System.out.println("operateur "+op+" non identifié");
 			   break;
 		}
-		
+		list = sortList(list, ascendingSort);
 		return list;
 	}
+	
+	private MyList sortList(MyList list, boolean ascendingSort)
+	{
+		ArrayList<Object> myList = list.getAt(2);
+		if(ascendingSort)
+		{
+			Collections.sort(myList, (o1, o2) -> ((Integer)o1).compareTo(((Integer)o2)));
+		}
+		else
+		{
+			Collections.sort(myList, (o1, o2) -> ((Integer)o2).compareTo(((Integer)o1)));
+		}
+		list.setAt(myList, 2);
+		return list;
+	}
+	
 }
